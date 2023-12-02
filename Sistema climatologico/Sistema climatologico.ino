bb201b1c9229381fb3aaec4a829e8a51d07ccd92
alex ronaldo san german carlin <84104241+ronaldo-carlin@users.noreply.github.com>
2023-12-02 12:41:54 -0600

    Update Sistema climatologico.ino

--- Sistema climatologico/Sistema climatologico.ino
+++ Sistema climatologico/Sistema climatologico.ino
@@ -1,35 +1,12 @@
-// Importar las bibliotecas necesarias
-#include <serial.h>
-#include <time.h>
-
-// Definir el nombre del archivo y abrirlo en modo escritura
-archivo = open('C:\\Users\\Admin\\OneDrive\\Documentos\\Arduino\\arduino.txt','w')
-
-// Inicializar el objeto de comunicación serial con el Arduino en el puerto COM10 y una velocidad de 9600 baudios
-serialArduino = serial.Serial("COM10", 9600)
-time.sleep(1)
-
-// Bucle principal
-while True:
-    // Leer una línea de datos desde el Arduino y decodificarla como ASCII
-    cad = serialArduino.readline().decode('ascii')
-    
-    // Imprimir la cadena recibida desde el Arduino en la consola
-    print(cad)
-
-    // Escribir la cadena en el archivo previamente abierto
-    archivo.write(cad)
-
 // Fotorecistencia
-// Definir el pin del LED y los pines de la fotocélula y el valor del sensor
 #define LED 3
 int FOTOPIN = A0;
 int valorSensor = 0;
 
 // Humedad
-// Definir el tipo de sensor DHT (DHT11), el pin al que está conectado y las variables para almacenar los datos
 #include <DHT.h>
 #include <DHT_U.h>
+
 #define Type DHT11
 int dhtPin = 4;
 DHT HT(dhtPin, Type);
@@ -38,97 +15,134 @@ float tempC;
 float tempF;
 int dth(500);
 
-// Rotación
-// Incluir las bibliotecas necesarias para controlar el MPU6050 a través de I2C
+// Rotación - MPU6050 (Acelerómetro y Giroscopio)
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "Wire.h"
 
-// Declarar el objeto MPU6050
 MPU6050 sensor;
 
-// Declarar variables para almacenar los valores crudos de aceleración y giroscopio
-int ax, ay, az;
-int gx, gy, gz;
+int ax, ay, az;  // Valores RAW (sin procesar) del acelerómetro en los ejes x, y, z
+int gx, gy, gz;  // Valores RAW (sin procesar) del giroscopio en los ejes x, y, z
 
-// Declarar variables para calcular los ángulos
 long tiempo_prev;
 float dt;
-float ang_x, ang_y;
+float ang_x, ang_y;  // Ángulos calculados con el acelerómetro
 float ang_x_prev, ang_y_prev;
 
-// Configuración inicial
+// Sensor de velocidad del viento
+#define VIENTO_PIN 2              // Pin digital al que está conectado el sensor de velocidad del viento
+#define INTERUPCIONES_POR_CM 4   // Número de interrupciones por cada 1 cm recorridos (ajusta según tu sensor)
+volatile int interrupciones = 0;  // Contador de interrupciones
+float distanciaRecorrida = 0;     // Distancia recorrida en cm
+float velocidadViento = 0;        // Velocidad del viento en km/h
+
 void setup() {
-    // Configuración de la fotocélula
-    pinMode(FOTOPIN, INPUT);
-    pinMode(LED, OUTPUT);
+  // Fotorecistencia
+  pinMode(FOTOPIN, INPUT);
+  pinMode(LED, OUTPUT);
 
-    // Configuración del sensor DHT
-    HT.begin();
+  // Humedad
+  HT.begin();
 
-    // Configuración del sensor MPU6050
-    Serial.begin(9600);
-    Wire.begin();
-    sensor.initialize();
+  // Rotación - MPU6050
+  Serial.begin(9600);   // Iniciando puerto serial
+  Wire.begin();         // Iniciando I2C
+  sensor.initialize();  // Iniciando el sensor
 
-    // Verificar si el sensor MPU6050 se ha iniciado correctamente
-    if (sensor.testConnection()) Serial.println("Sensor iniciado correctamente");
-    else Serial.println("Error al iniciar el sensor");
+  if (sensor.testConnection()) {
+    Serial.println("Sensor iniciado correctamente");
+  } else {
+    Serial.println("Error al iniciar el sensor");
+  }
+
+  // Configurar el sensor de velocidad del viento
+  pinMode(VIENTO_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(VIENTO_PIN), contarInterrupciones, RISING);
 }
 
-// Bucle principal
 void loop() {
-    // Lectura de la fotocélula
-    valorSensor = analogRead(FOTOPIN);
-    Serial.print("Valor del Sensor: ");
+  // Fotorecistencia
+  valorSensor = analogRead(FOTOPIN);
+  Serial.print("Valor del Sensor: ");
+  Serial.println(valorSensor);
+
+  if (valorSensor <= 30) {    // un valor bajo representa oscuridad
+    digitalWrite(LED, HIGH);  // enciende LED
+    delay(1000);              // demora de 1 seg. para evitar parpadeo de LED
+    Serial.println("NOCHE");
+    Serial.print("Valor: ");
     Serial.println(valorSensor);
+  } else {
+    Serial.println("DIA");
+  }
+  digitalWrite(LED, LOW);
 
-    // Control del LED en función del valor de la fotocélula
-    if (valorSensor <= 30) {
-        digitalWrite(LED, HIGH);
-        delay(1000);
-        Serial.print("Valor: ");
-        Serial.println(valorSensor);
-        Serial.print("NOCHE");
-    }
-    Serial.print("DIA");
-    digitalWrite(LED, LOW);
+  // Humedad
+  humidity = HT.readHumidity();
+  tempC = HT.readTemperature();
+  tempF = HT.readTemperature(true);
+  Serial.print("Humedad Relativa: ");
+  Serial.print(humidity);
+  Serial.print("% / Temperatura: ");
+  Serial.print(tempC);
+  Serial.print("ºC / ");
+  Serial.print(tempF);
+  Serial.println("ºF");
+  delay(dth);
 
-    // Lectura de la humedad y temperatura
-    humidity = HT.readHumidity();
-    tempC = HT.readTemperature();
-    tempF = HT.readTemperature(true);
-    Serial.print("Humedad Relativa: ");
-    Serial.print(humidity);
-    Serial.print("% / Temperatura: ");
-    Serial.print(tempC);
-    Serial.print("ºC / ");
-    Serial.print(tempF);
-    Serial.println("ºF");
-    delay(dth);
+  // Rotación - MPU6050
+  sensor.getAcceleration(&ax, &ay, &az);
+  sensor.getRotation(&gx, &gy, &gz);
 
-    // Lectura de la rotación utilizando el sensor MPU6050
-    sensor.getAcceleration(&ax, &ay, &az);
-    sensor.getRotation(&gx, &gy, &gz);
-    
-    // Cálculos para obtener los ángulos de rotación
-    dt = (millis()-tiempo_prev)/1000.0;
-    tiempo_prev = millis();
-    
-    float accel_ang_x = atan(ay/sqrt(pow(ax,2) + pow(az,2)))*(180.0/3.14);
-    float accel_ang_y = atan(-ax/sqrt(pow(ay,2) + pow(az,2)))*(180.0/3.14);
-  
-    ang_x = 0.98*(ang_x_prev+(gx/131)*dt) + 0.02*accel_ang_x;
-    ang_y = 0.98*(ang_y_prev+(gy/131)*dt) + 0.02*accel_ang_y;
-  
-    ang_x_prev = ang_x;
-    ang_y_prev = ang_y;
+  dt = (millis() - tiempo_prev) / 1000.0;
+  tiempo_prev = millis();
 
-    // Mostrar los ángulos en la consola
-    Serial.print("Rotacion en X:  ");
-    Serial.print(ang_x); 
-    Serial.print(" Rotacion en Y: ");
-    Serial.println(ang_y);
+  // Calcular los ángulos con acelerómetro
+  float accel_ang_x = atan(ay / sqrt(pow(ax, 2) + pow(az, 2))) * (180.0 / 3.14);
+  float accel_ang_y = atan(-ax / sqrt(pow(ay, 2) + pow(az, 2))) * (180.0 / 3.14);
 
-    delay(1000);
+  // Calcular el ángulo de rotación con giroscopio y filtro complemento
+  ang_x = 0.98 * (ang_x_prev + (gx / 131) * dt) + 0.02 * accel_ang_x;
+  ang_y = 0.98 * (ang_y_prev + (gy / 131) * dt) + 0.02 * accel_ang_y;
+
+  ang_x_prev = ang_x;
+  ang_y_prev = ang_y;
+
+  // Mostrar los ángulos separados por un [tab]
+  Serial.print("Rotacion en X:  ");
+  Serial.print(ang_x);
+  Serial.print("\tRotacion en Y: ");
+  Serial.println(ang_y);
+
+  // Mostrar el número de interrupciones realizadas en este ciclo
+  Serial.print("Interrupciones en este ciclo: ");
+  Serial.println(interrupciones);
+
+  // Calcular distancia recorrida y velocidad del viento
+  distanciaRecorrida = interrupciones / INTERUPCIONES_POR_CM;
+  velocidadViento = distanciaRecorrida * (3600.0 / 100000);  // Convertir a km/h
+
+  // Mostrar la velocidad del viento
+  Serial.print("Velocidad del Viento: ");
+  Serial.print(velocidadViento);
+  Serial.println(" km/h");
+
+  // Reiniciar el contador de interrupciones y la distancia
+  interrupciones = 0;
+  distanciaRecorrida = 0;
+
+  delay(1000);
 }
+
+void contarInterrupciones() {
+  interrupciones++;
+}
+
+// Conexiones Físicas:
+// - Fotorecistencia (LDR) conectada al pin analógico A0.
+// - LED conectado al pin digital 3.
+// - Sensor de Humedad (DHT11) conectado al pin digital 4.
+// - MPU6050 (Acelerómetro y Giroscopio) conectado a través de I2C (SCL y SDA).
+// - Sensor de Velocidad del Viento conectado al pin digital 2 para las interrupciones.
+//   Asegúrate de conectar correctamente la alimentación y la tierra según las especificaciones de cada sensor.
